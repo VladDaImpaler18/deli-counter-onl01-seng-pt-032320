8391941be650f0f549da996816cb8e11d140d6af
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-31 05:39:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+# (c) 2020 Vladimir Jimenez, all rights reserved
+# For Online Software Engineering PT - First Mile
+
+def line(queue)
+  numberInLine = 1 
+  queue.empty? ? puts "The line is currently empty.": queue.each{ |name| puts "Welcome, #{name}. You are number #{numberInLine} in line." numberInLine+=1 }
+end
+
+def 
