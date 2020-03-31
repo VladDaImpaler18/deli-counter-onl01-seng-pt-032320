0b3cfbb9d9ab3df935f848e1a2ae4c76e5ac6d61
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-31 06:08:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,19 @@
 # For Online Software Engineering PT - First Mile
 
 def line(queue)
-  numberInLine = 1 
-  queue.empty? ? puts "The line is currently empty.": queue.each{ |name| puts  numberInLine+=1 }
+  numberInLine = 1
+  statement="The line is currently: "
+  queue.empty? ? puts "The line is currently empty.": queue.each{ |name| 
+    statement << "#{numberInLine}. #{name} "unless name==queue.last
+    statement << "#{numberInLine}. #{name}" if name==queue.last"
+  }
 end
 
 def take_a_number(queue,customerName)
   queue.push(customerName)
-  puts "Welcome, #{name}. You are number  in line."
+  puts "Welcome, #{array.last}. You are number #{array.size} in line."
+end
+
+def now_serving(queue)
+  puts "There is nobody waiting to be served!" break if queue.empty?
 end
